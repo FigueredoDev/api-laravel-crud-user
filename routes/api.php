6ea2be9d059f8b3bd83feeda3428728f4a6752b2
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-19 17:51:59 -0300

    refactor(routes): consolidate user routes with apiResource

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,7 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get("/users/{id}", [UserController::class, 'show'])->name('users.show');
-Route::get("/users", [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::apiResource('users', UserController::class);
 
 Route::get('/', function () {
     return response()->json([
